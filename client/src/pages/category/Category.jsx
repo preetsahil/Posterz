bad7f8240b7101082d657264c3ec97c9a5ebca3b
Sahil <preetsahil289@gmail.com>
2024-04-21 17:34:40 +0530

    initialize category page

--- client/src/pages/category/Category.jsx
+++ client/src/pages/category/Category.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Category.scss";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { FaPlus } from "react-icons/fa6";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { TiTick } from "react-icons/ti";
+import { IoMdArrowDropup } from "react-icons/io";
+import { IoMdArrowDropdown } from "react-icons/io";
 
 function Category() {
   const navigate = useNavigate();
   const location = useLocation();
+  const [fill, setFill] = useState(false);
+  const [up, setUp] = useState(false);
+  const [down, setDown] = useState(false);
+  const [visId, setVisId] = useState(false);
+  const [visTitle, setVisTitle] = useState(false);
+  const [visKey, setVisKey] = useState(false);
   const categories = useSelector((state) => state.categoryReducer.categories);
+
+  const fillAllCheck = () => {
+    setFill(!fill);
+  };
 
   return (
     <div className="cat">
@@ -44,10 +57,117 @@ function Category() {
               </div>
             </div>
           </div>
+          <div className="cat-list">
+        <div className="head">
+          <div
+            className={fill ? "closeCheck" : "openCheck"}
+            onClick={fillAllCheck}
+          >
+            <TiTick className="tick" />
+          </div>
+          <div className="heading">
+            <div className="id">
+              <p
+                onClick={() => {
+                  if (!visId) {
+                    setDown(false);
+                    setUp(false);
+                    setVisId(true);
+                    setVisTitle(false);
+                    setVisKey(false);
+                  }
+
+                  if (!up && !down) {
+                    setUp(true);
+                  }
+                  if (up && !down) {
+                    setUp(false);
+                    setDown(true);
+                  }
+                  if (!up && down) {
+                    setUp(true);
+                    setDown(false);
+                  }
+                }}
+              >
+                ID
+              </p>
+              <IoMdArrowDropup className={up & visId ? "up" : "none"} />
+              <IoMdArrowDropdown className={down & visId ? "down" : "none"} />
+            </div>
+            <div className="title">
+              <p
+                onClick={() => {
+                  if (!visTitle) {
+                    setDown(false);
+                    setUp(false);
+                    setVisTitle(true);
+                    setVisId(false);
+                    setVisKey(false);
+                  }
+
+                  if (!up && !down) {
+                    setUp(true);
+                  }
+                  if (up && !down) {
+                    setUp(false);
+                    setDown(true);
+                  }
+                  if (!up && down) {
+                    setUp(true);
+                    setDown(false);
+                  }
+                }}
+              >
+                TITLE
+              </p>
+              <IoMdArrowDropup className={up & visTitle ? "up" : "none"} />
+              <IoMdArrowDropdown
+                className={down & visTitle ? "down" : "none"}
+              />
+            </div>
+            <div
+              className="key"
+             
+            >
+              <p  onClick={() => {
+                if (!visKey) {
+                  setDown(false);
+                  setUp(false);
+                  setVisKey(true);
+                  setVisTitle(false);
+                  setVisId(false);
+                }
+
+                if (!up && !down) {
+                  setUp(true);
+                }
+                if (up && !down) {
+                  setUp(false);
+                  setDown(true);
+                }
+                if (!up && down) {
+                  setUp(true);
+                  setDown(false);
+                }
+              }}>KEY</p>
+              <IoMdArrowDropup className={up & visKey ? "up" : "none"} />
+              <IoMdArrowDropdown className={down & visKey ? "down" : "none"} />
+            </div>
+            <div className="image">
+              <p>IMAGE</p>
+            </div>
+            <div className="products">
+              <p>N-Products</p>
+            </div>
+          </div>
+        </div>
+      </div>
         </div>
       ) : (
         <Outlet />
       )}
+    
     </div>
   );
 }
