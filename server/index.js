a5623bc6ad061fb5010595afc5f0bde97c7da9d9
Sahil <preetsahil289@gmail.com>
2024-07-03 12:02:49 +0530

    deply8

--- server/index.js
+++ server/index.js
@@ -21,10 +21,7 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
+  res.header("Access-Control-Allow-Origin", "https://posterz-ten.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", true);
