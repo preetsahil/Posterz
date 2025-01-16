c4b70abf51856c018ad1800bba843981170f1064
preetsahil <preetsahil289@gmail.com>
2025-01-17 01:37:48 +0530

    changed logic in cronjob

--- server/utils/updateForTopPick.js
+++ server/utils/updateForTopPick.js
@@ -2,56 +2,64 @@ const Order = require("../models/Order");
 const Product = require("../models/Product");
 
 const updateForTopPick = async () => {
-  const orders = await Order.find({ order_status: "success" });
-  let productStats = {};
+    const stats = await Order.aggregate([
+      { $match: { order_status: "success" } },
+      { $unwind: "$item" },
+      {
+        $group: {
+          _id: null,
+          totalRevenue: {
+            $sum: { $multiply: ["$item.price", "$item.quantity"] },
+          },
+          productStats: {
+            $push: {
+              title: "$item.title",
+              quantity: "$item.quantity",
+              revenue: { $multiply: ["$item.price", "$item.quantity"] },
+            },
+          },
+        },
+      },
+    ]);
+    const productStats = {};
+    stats[0].productStats.forEach(({ title, quantity, revenue }) => {
+      if (!productStats[title]) {
+        productStats[title] = { totalQuantity: 0, totalRevenue: 0 };
+      }
+      productStats[title].totalQuantity += quantity;
+      productStats[title].totalRevenue += revenue;
+    });
 
-  await Promise.all(
-    orders.map(async (order) => {
-      await Promise.all(
-        order.item.map(async (product) => {
-          const quantity = product.quantity;
-          const price = product.price;
-          const revenue = quantity * price;
+    const findMostFrequent = (map, key) => {
+      let maxValue = -Infinity;
+      let mostFrequent = [];
 
-          const prod = await Product.findOne({ title: product.title });
-          if (prod) {
-            if (!productStats[product.title]) {
-              productStats[product.title] = {
-                totalQuantity: 0,
-                totalRevenue: 0,
-              };
-            }
-            productStats[product.title].totalQuantity += quantity;
-            productStats[product.title].totalRevenue += revenue;
-          }
-        })
-      );
-    })
-  );
-
-  const findMaxOrderedProduct = (stats, key) => {
-    const max = Math.max(...Object.values(stats).map((stat) => stat[key]));
-    return Object.keys(stats).filter(
-      (category) => stats[category][key] === max
-    );
-  };
-
-  const mostFrequentProducts = findMaxOrderedProduct(
-    productStats,
-    "totalQuantity"
-  );
-
-  await Promise.all(
-    mostFrequentProducts.map(async (product) => {
-      if (productStats[product].totalQuantity > 10) {
-        const prod = await Product.findOne({ title: product });
-        if (prod && !prod.isTopPick) {
-          prod.isTopPick = true;
-          await prod.save();
+      for (const stat in map) {
+        if (map[stat][key] > maxValue) {
+          maxValue = map[stat][key];
+          mostFrequent = [stat];
+        } else if (map[stat][key] === maxValue) {
+          mostFrequent.push(stat);
         }
       }
-    })
-  );
-};
 
+      return mostFrequent;
+    };
+
+    const productFrequency = findMostFrequent(
+      productStats,
+      "totalQuantity"
+    );
+
+    const updateTopPickStatus = async (product) => {
+      const prod = await Product.findOne({ title: product });
+      if (prod && productStats[product].totalQuantity > 10 && !prod.isTopPick) {
+        prod.isTopPick = true;
+        await prod.save();
+      }
+    };
+    await Promise.all(
+      productFrequency.map(updateTopPickStatus)
+    );
+  }
 module.exports = updateForTopPick;
